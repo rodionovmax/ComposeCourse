716edcd1e22bb458dea8fca8ceb88f6605296b33
rodionovmax <rodionovmax90test@gmail.com>
2023-07-05 00:14:48 -0400

    part 27

--- app/src/main/java/com/example/composecourse/MainActivity.kt
+++ app/src/main/java/com/example/composecourse/MainActivity.kt
@@ -9,8 +9,11 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -20,13 +23,79 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composecourse.ui.theme.ComposeCourseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            ComposeCourseTheme {
+                val windowInfo = rememberWindowInfo()
+                if (windowInfo.screenWidthInfo is WindowInfo.WindowType.Compact) {
+                    LazyColumn(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
+                        // List 1
+                        items(10) {
+                            Text(
+                                text = "Item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.Cyan)
+                                    .padding(16.dp)
+                            )
+                        }
+                        // List 2
+                        items(10) {
+                            Text(
+                                text = "Item $it",
+                                fontSize = 25.sp,
+                                modifier = Modifier
+                                    .fillMaxWidth()
+                                    .background(Color.Green)
+                                    .padding(16.dp)
+                            )
+                        }
+                    }
+                } else {
+                    Row(
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        LazyColumn(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            // List 1
+                            items(10) {
+                                Text(
+                                    text = "Item $it",
+                                    fontSize = 25.sp,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(Color.Cyan)
+                                        .padding(16.dp)
+                                )
+                            }
+                        }
+                        LazyColumn(
+                            modifier = Modifier.weight(1f)
+                        ) {
+                            // List 2
+                            items(10) {
+                                Text(
+                                    text = "Item $it",
+                                    fontSize = 25.sp,
+                                    modifier = Modifier
+                                        .fillMaxWidth()
+                                        .background(Color.Green)
+                                        .padding(16.dp)
+                                )
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
